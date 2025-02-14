98cde6959deac06d772fd37970347ebb18a088e2
SujalSinghNegi <negisujal2007@gmail.com>
2025-02-14 15:43:57 +0530

    added the new file in the recursion/Count numbers.cpp

---
+++ recursion/Count numbers.cpp
@@ -0,0 +1,4 @@
+// Q - give a n, at the odd index of n lenght string we can add the prime num- 2,3,5,7 and at the even index, we can add, 0,2,4,6,8, 
+// count the total posible n length string
+// It is the Implementation of the power function
+
