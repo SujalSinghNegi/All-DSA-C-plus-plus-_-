2212dbeffd86bfadf8db958be2aa7f7e036fb39e
SujalSinghNegi <negisujal2007@gmail.com>
2025-02-14 12:22:37 +0530

    described about the BST iterator

---
+++ tree/BST iterator.cpp
@@ -0,0 +1,5 @@
+// here implementing the how to put a iterator in a BST
+// like in the array we put pointer in the first index or at the last index
+// similarly on the Inorder traversal of BST , if I put a iterator on the leftest index and then move the inorder then it will give the 
+// elements in the ascending order , as i requested, not all elements in one time, 
+// and if i put the iterator on the rightest, or say i reverse the procedure, i get the descending elements , in the reverse inorder
