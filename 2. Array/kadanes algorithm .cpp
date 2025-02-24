93e99d8848a9f8785cd6401b419ad2cbc68ad113
sujal singh negi <negisujal2007@gmail.com>
2025-02-24 21:38:44 +0530

    kadne's algo

---
+++ 2. Array/kadanes algorithm .cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+   int maxSubarraySum(vector<int> &arr) {
+        int count=INT_MIN,sum=0,n=arr.size();
+        for(int i=0;i<n;i++)
+        {
+            sum+=arr[i];
+            count=max(count,sum);
+            if(sum<0)
+            sum=0;
+        }
+        return count;
+    }
+};
