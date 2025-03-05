bc092db671cb5f4303fdcc3c8b1b5df29fe249a6
sujal singh negi <negisujal2007@gmail.com>
2025-03-05 12:55:27 +0530

    added answer3

---
+++ 1. Basic Learning/Answer3.java
@@ -0,0 +1,10 @@
+// take the user age , and print the age in years , months, days
+class Answer3{
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the number of elements in the array");
+        int n = s.nextInt();
+        System
+        
+    }
+}
